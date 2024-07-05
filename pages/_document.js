e2c9f858c98a625e306671ae05b0bc8ecb0ba23a
Loren Gardner <gardner.loren@gmail.com>
2024-07-05 08:08:51 -0400

    Stylesheets added to _document.js
    Added link to bootstrap.css
    Moved globals.css to new css folder within public, added link to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" href="../css/globals.css"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
