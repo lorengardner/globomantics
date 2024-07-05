26c7e14cd3ffb08c16ee4e62cb79ad20d89756bd
Loren Gardner <gardner.loren@gmail.com>
2024-07-05 14:26:40 -0400

    Fragments and Mapping Data to JSX
    Created new File "houseList.js" that renders a new component to display an array of houses mapped to rows in a table.

--- components/app.js
+++ components/app.js
@@ -1,17 +1,19 @@
 import Banner from './banner'
+import HouseList from './houseList';
 
 const jsx = <div>Hi</div>
 
 const App = () => {
     return (
-    <div>
+    <>
         {/* {jsx} This shows an example of a jsx string assigned to a constant and then displayed*/}
         <Banner>
             <div>
                 Providing houses all over the world.
             </div>
         </Banner>
-    </div>
+        <HouseList/>
+    </>
     )
 }
 
