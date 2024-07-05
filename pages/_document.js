b1d34489b842b92b6042a537ad85160fc3c8b692
Loren Gardner <gardner.loren@gmail.com>
2024-07-05 08:38:14 -0400

    Update _document.js
    Added <div> around main with className "container" from bootstrap.

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,16 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
         />
-        <link rel="stylesheet" href="../css/globals.css"/>
+        <link 
+          rel="stylesheet" 
+          href="css/globals.css"
+        />
       </Head>
       <body>
-        <Main />
+        <div className="container">
+          <Main />
+        </div>
+        
         <NextScript />
       </body>
     </Html>
